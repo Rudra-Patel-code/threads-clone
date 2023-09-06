a458076e3f216cd7d3f28684583cd1144c1b5daf
Rudra Patel <rudracode2004@gmail.com>
2023-09-06 17:44:53 +0530

    second commit

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex min-h-screen min-w-full justify-center items-center text-white">
+      loader...
+    </div>
+  );
+};
+
+export default loading;
